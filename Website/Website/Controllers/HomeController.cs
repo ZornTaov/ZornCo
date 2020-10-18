85c94ecdfa5e91aff6608f0bb219cfb9bdb04c1f
ZornTaov <zeke_kjell2@yahoo.com>
2020-10-17 23:36:16 -0700

    Implemented basic JSON loader to Project Page

--- Website/Website/Controllers/HomeController.cs
+++ Website/Website/Controllers/HomeController.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
 using Microsoft.Extensions.Logging;
 using Website.Models;
+using Website.Json;
+using Newtonsoft.Json;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Website.Controllers
 {
@@ -13,10 +20,17 @@ namespace Website.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IWebHostEnvironment _env;
+        private readonly List<ProjectJson> projects;
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment env)
         {
             _logger = logger;
+            _env = env;
+
+            using (StreamReader sr = new StreamReader(Path.Combine(_env.WebRootPath, "projects/project.json")))
+            {
+                projects = JsonConvert.DeserializeObject<List<ProjectJson>>(sr.ReadToEnd());
+            }
         }
 
         public IActionResult Index()
@@ -28,8 +42,15 @@ namespace Website.Controllers
         [Route("project/{id}")]
         public IActionResult Project(string id)
         {
-            ViewBag.ProjectID = id;
-            return View();
+            var projectvar = from f in projects where f.Id == id select f;
+            ProjectJson project = projectvar.FirstOrDefault();
+            if (project == null)
+            {
+                //TODO: Redirect to "project not found" page
+                return RedirectToAction(actionName: nameof(Index));
+            }
+
+            return View(project);
         }
 
         [Route("error")]
---
+++ Website/Website/Json/ProjectJson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace Website.Json
+{
+    [JsonObject]
+    public class ProjectJson
+    {
+        [JsonProperty("id")]
+        public string Id;
+        [JsonProperty("name")]
+        public string Name;
+        [JsonProperty("images")]
+        public string[] Images;
+        [JsonProperty("semester")]
+        public string Semester;
+        [JsonProperty("about")]
+        public string About;
+        [JsonProperty("whatidid")]
+        public string WhatIDid;
+    }
+}
